e6e427be42f424e5b8a538662961a8e4c1908340
June Paolo Tabequero <june.ceosi@gmail.com>
2022-10-29 06:39:13 +0800

    Add. sidebar widget/screen

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
-final primaryColor = const Color(0xFF293462);
-final secondaryColor = const Color(0xFFD61C4A);
-final greyAccent = const Color(0xFFD9D9D9);
+const primaryColor = Color(0xFF293462);
+const secondaryColor = Color(0xFFD61C4A);
+const greyAccent = Color(0xFFD9D9D9);
--- lib/constants/labels.dart
+++ lib/constants/labels.dart
@@ -1,6 +1,6 @@
 class Label {
-  static const CeosiFlutterCatalog = 'CEOSI Flutter Catalog',
-      CeosiFreedomWall = 'CEOSI Freedom Wall',
-      CeosiCompanyApp = 'CEOSI Company App',
-      CeosiRewards = 'CEOSI Rewards';
+  static const ceosiFlutterCatalog = 'CEOSI Flutter Catalog',
+      ceosiFreedomWall = 'CEOSI Freedom Wall',
+      ceosiCompanyApp = 'CEOSI Company App',
+      ceosiRewards = 'CEOSI Rewards';
 }
