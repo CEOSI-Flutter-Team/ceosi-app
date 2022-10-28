1d10e14d0aa4bee1ce69207e348f8d30bacf877f
BiscochoChloie <chloiebiscocho123@gmail.com>
2022-10-28 15:02:08 +0800

    Fix. Make logo and names changeable

--- lib/constants/images.dart
+++ lib/constants/images.dart
@@ -1,11 +1,11 @@
 const coesiIcon = 'assets/icons/logo.png';
 
-const coesiLogo1 = 'assets/images/FINAL-LOGO-1.0.1.png';
+const coesiLogoCompleteAndWhiteText = 'assets/images/FINAL-LOGO-1.0.1.png';
 
-const coesiLogo2 = 'assets/images/FINAL-LOGO-1.0.png';
+const coesiLogoCompleteAndMaroonBlueText = 'assets/images/FINAL-LOGO-1.0.png';
 
-const coesiLogo3 = 'assets/images/FINAL-LOGO-1.1.png';
+const coesiLogoMaroonBlue = 'assets/images/FINAL-LOGO-1.1.png';
 
-const coesiLogo4 = 'assets/images/FINAL-LOGO-1.2.png';
+const coesiLogoMaroonText = 'assets/images/FINAL-LOGO-1.2.png';
 
-const coesiLogo5 = 'assets/images/FINAL-LOGO-1.3.png';
+const coesiLogoWhiteText = 'assets/images/FINAL-LOGO-1.3.png';
