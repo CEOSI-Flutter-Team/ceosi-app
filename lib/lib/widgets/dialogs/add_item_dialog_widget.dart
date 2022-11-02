45e1483d9c6e7e377ee9f1a15230dd04467cd294
123Lance-Olana <lance.ceosi@gmail.com>
2022-11-02 08:26:22 +0800

    Add. search delegate

--- lib/lib/widgets/dialogs/add_item_dialog_widget.dart
+++ lib/lib/widgets/dialogs/add_item_dialog_widget.dart
@@ -4,8 +4,9 @@ import 'package:ceosi_app/lib/widgets/textformfield_widget.dart';
 import 'package:flutter/material.dart';
 
 import '../buttons/button_widget.dart';
+import '../buttons/dropdown_item_widget.dart';
 
-class AddItemDialogWidget extends StatelessWidget {
+class AddItemDialogWidget extends StatefulWidget {
   late var itemNameController = TextEditingController();
   late var pointsEqualController = TextEditingController();
 
@@ -13,6 +14,15 @@ class AddItemDialogWidget extends StatelessWidget {
     required this.itemNameController,
     required this.pointsEqualController,
   });
+
+  @override
+  State<AddItemDialogWidget> createState() => _AddItemDialogWidgetState();
+}
+
+class _AddItemDialogWidgetState extends State<AddItemDialogWidget> {
+  int _dropdownValue = 0;
+
+  String itemCategory = 'Snacks';
 
   @override
   Widget build(BuildContext context) {
@@ -52,12 +62,12 @@ class AddItemDialogWidget extends StatelessWidget {
                   label: 'Item Name',
                   isObscure: false,
                   colorFill: Colors.white,
-                  textFieldController: itemNameController),
+                  textFieldController: widget.itemNameController),
               TextformfieldWidget(
                   label: 'Coins Equivalent',
                   isObscure: false,
                   colorFill: Colors.white,
-                  textFieldController: pointsEqualController),
+                  textFieldController: widget.pointsEqualController),
               const SizedBox(
                 height: 10,
               ),
@@ -73,25 +83,45 @@ class AddItemDialogWidget extends StatelessWidget {
                   decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.circular(100)),
-                  child: Row(
-                    children: [
-                      const SizedBox(
-                        width: 20,
+                  child: SizedBox(
+                    width: 250,
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 2, 20, 2),
+                      child: DropdownButton(
+                        underline: Container(color: Colors.transparent),
+                        iconEnabledColor: Colors.black,
+                        isExpanded: true,
+                        value: _dropdownValue,
+                        items: [
+                          DropdownMenuItem(
+                            onTap: () {
+                              itemCategory = "Snacks";
+                            },
+                            value: 0,
+                            child: DropDownItem(label: 'Snacks'),
+                          ),
+                          DropdownMenuItem(
+                            onTap: () {
+                              itemCategory = "Drinks";
+                            },
+                            value: 1,
+                            child: DropDownItem(label: 'Drinks'),
+                          ),
+                          DropdownMenuItem(
+                            onTap: () {
+                              itemCategory = "Candies";
+                            },
+                            value: 2,
+                            child: DropDownItem(label: 'Candies'),
+                          ),
+                        ],
+                        onChanged: (value) {
+                          setState(() {
+                            _dropdownValue = int.parse(value.toString());
+                          });
+                        },
                       ),
-                      const NormalTextWidget(
-                          color: primaryColor, fontSize: 12, text: 'Snacks'),
-                      const Expanded(child: SizedBox()),
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.arrow_drop_down_circle_rounded,
-                          color: primaryColor,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                    ],
+                    ),
                   ),
                 ),
               ),
