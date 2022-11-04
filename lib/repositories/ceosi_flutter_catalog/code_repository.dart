d95d320c5c96313cad1d19fb3c1a9994621ea12c
June Paolo Tabequero <june.ceosi@gmail.com>
2022-11-04 11:22:35 +0800

    Add. source code screen

--- lib/repositories/ceosi_flutter_catalog/code_repository.dart
+++ lib/repositories/ceosi_flutter_catalog/code_repository.dart
@@ -1,3 +1,4 @@
+import 'package:ceosi_app/models/ceosi_flutter_catalog/code_model.dart';
 import 'package:ceosi_app/repositories/ceosi_flutter_catalog/code_repository_interface.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,31 +7,139 @@ final codeRepositoryProvider =
 
 class CodeRepository implements CodeRepositoryInterface {
   @override
-  Future<Map<String, dynamic>?> getCodeList() async {
-    final data = await Future.value({
-      "code_list": [
-        {
-          "title": "Code Sample 1",
-          "category": "Category 1",
-        },
-        {
-          "title": "Code Sample 2",
-          "category": "Category 4",
-        },
-        {
-          "title": "Code Sample 3",
-          "category": "Category 2",
-        },
-        {
-          "title": "Code Sample 4",
-          "category": "Category 8",
-        },
-        {
-          "title": "Code Sample 5",
-          "category": "Category 1",
-        },
-      ],
-    });
+  Future<CodeModel?> getCodeData() async {
+    final data = await Future.value(
+      CodeModel(
+        codeData: [
+          CodeDatum(
+              title: 'Code Sample 1',
+              category: 'Category 1',
+              description: sampleDescription,
+              isCode: true,
+              data: sampleSourceCode),
+          CodeDatum(
+              title: 'Code Sample 2',
+              category: 'Category 4',
+              description: sampleDescription,
+              isCode: false,
+              data: sampleImage),
+          CodeDatum(
+              title: 'Code Sample 3',
+              category: 'Category 2',
+              description: sampleDescription,
+              isCode: true,
+              data: sampleSourceCode),
+          CodeDatum(
+              title: 'Code Sample 4',
+              category: 'Category 8',
+              description: sampleDescription,
+              isCode: true,
+              data: sampleSourceCode),
+          CodeDatum(
+              title: 'Code Sample 5',
+              category: 'Category 1',
+              description: sampleDescription,
+              isCode: false,
+              data: sampleImage),
+        ],
+      ),
+    );
     return Future.delayed(const Duration(seconds: 3), () => data);
   }
+
+  String sampleDescription =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.\nLorem ipsum dolor sit amet, consectetur adipiscing elit.\nLorem ipsum dolor sit amet, consectetur adipiscing elit.\nLorem ipsum dolor sit amet, consectetur adipiscing elit.';
+
+  String sampleImage =
+      'https://media.geeksforgeeks.org/wp-content/uploads/20200904104133/Screenshot633.png';
+
+  String sampleSourceCode = """
+``````dart
+class SourceCodeScreen extends StatelessWidget {
+  const SourceCodeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final args = ModalRoute.of(context)!.settings.arguments as SourceCodeArguments;
+    const markdownSource = \"\"\"
+```dart
+class SourceCodeScreen extends StatelessWidget {
+  const SourceCodeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final args = ModalRoute.of(context)!.settings.arguments as SourceCodeArguments;
+
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: BoldTextWdiget(
+          color: Colors.white, fontSize: 14.0, text: args.title),
+        centerTitle: true,
+        backgroundColor: CustomColors.primary,
+        drawer: const SidebarWidget(),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 20.0),
+                child: RichText(
+                  overflow: TextOverflow.clip,
+                  textAlign: TextAlign.start,
+                  textDirection: TextDirection.ltr,
+                  softWrap: true,
+                  text: TextSpan(
+                    text:
+                        ''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.\nLorem ipsum dolor sit amet, consectetur adipiscing elit.\nLorem ipsum dolor sit amet, consectetur adipiscing elit.\nLorem ipsum dolor sit amet, consectetur adipiscing elit.'',
+                    style: GoogleFonts.aleo(
+                        color: Colors.black, fontSize: 12.0, height: 1.5),
+                )),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+```
+
+\"\"\";
+
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: BoldTextWdiget(
+          color: Colors.white, fontSize: 14.0, text: args.title),
+        centerTitle: true,
+        backgroundColor: CustomColors.primary,
+        drawer: const SidebarWidget(),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 20.0),
+                child: RichText(
+                  overflow: TextOverflow.clip,
+                  textAlign: TextAlign.start,
+                  textDirection: TextDirection.ltr,
+                  softWrap: true,
+                  text: TextSpan(
+                    text:
+                        ''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.\nLorem ipsum dolor sit amet, consectetur adipiscing elit.\nLorem ipsum dolor sit amet, consectetur adipiscing elit.\nLorem ipsum dolor sit amet, consectetur adipiscing elit.'',
+                    style: GoogleFonts.aleo(
+                        color: Colors.black, fontSize: 12.0, height: 1.5),
+                )),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+``````
+
+""";
 }
--- lib/screens/ceosi_flutter_catalog/source_code_screen.dart
+++ lib/screens/ceosi_flutter_catalog/source_code_screen.dart
@@ -1,5 +1,8 @@
+import 'package:ceosi_app/models/ceosi_flutter_catalog/code_model.dart';
+import 'package:ceosi_app/providers/ceosi_flutter_catalog/code_provider.dart';
 import 'package:ceosi_app/widgets/button_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:markdown_widget/markdown_widget.dart';
 import 'package:markdown_widget/config/highlight_themes.dart' as theme;
@@ -16,95 +19,6 @@ class SourceCodeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final args =
         ModalRoute.of(context)!.settings.arguments as SourceCodeArguments;
-    const markdownSource = """
-``````dart
-class SourceCodeScreen extends StatelessWidget {
-  const SourceCodeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final args = ModalRoute.of(context)!.settings.arguments as SourceCodeArguments;
-    const markdownSource = \"\"\"
-```dart
-class SourceCodeScreen extends StatelessWidget {
-  const SourceCodeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final args = ModalRoute.of(context)!.settings.arguments as SourceCodeArguments;
-
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        title: BoldTextWdiget(
-          color: Colors.white, fontSize: 14.0, text: args.title),
-        centerTitle: true,
-        backgroundColor: CustomColors.primary,
-        drawer: const SidebarWidget(),
-        body: Column(
-          children: <Widget>[
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 20.0),
-                child: RichText(
-                  overflow: TextOverflow.clip,
-                  textAlign: TextAlign.start,
-                  textDirection: TextDirection.ltr,
-                  softWrap: true,
-                  text: TextSpan(
-                    text:
-                        ''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.\nLorem ipsum dolor sit amet, consectetur adipiscing elit.\nLorem ipsum dolor sit amet, consectetur adipiscing elit.\nLorem ipsum dolor sit amet, consectetur adipiscing elit.'',
-                    style: GoogleFonts.aleo(
-                        color: Colors.black, fontSize: 12.0, height: 1.5),
-                )),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-```
-
-\"\"\";
-
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        title: BoldTextWdiget(
-          color: Colors.white, fontSize: 14.0, text: args.title),
-        centerTitle: true,
-        backgroundColor: CustomColors.primary,
-        drawer: const SidebarWidget(),
-        body: Column(
-          children: <Widget>[
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 20.0),
-                child: RichText(
-                  overflow: TextOverflow.clip,
-                  textAlign: TextAlign.start,
-                  textDirection: TextDirection.ltr,
-                  softWrap: true,
-                  text: TextSpan(
-                    text:
-                        ''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.\nLorem ipsum dolor sit amet, consectetur adipiscing elit.\nLorem ipsum dolor sit amet, consectetur adipiscing elit.\nLorem ipsum dolor sit amet, consectetur adipiscing elit.'',
-                    style: GoogleFonts.aleo(
-                        color: Colors.black, fontSize: 12.0, height: 1.5),
-                )),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-``````
-
-""";
 
     return Scaffold(
       appBar: AppBar(
@@ -115,104 +29,119 @@ class SourceCodeScreen extends StatelessWidget {
         backgroundColor: CustomColors.primary,
       ),
       drawer: const SidebarWidget(),
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 20.0),
-            child: RichText(
-                overflow: TextOverflow.clip,
-                textAlign: TextAlign.start,
-                textDirection: TextDirection.ltr,
-                softWrap: true,
-                text: TextSpan(
-                  text:
-                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.\nLorem ipsum dolor sit amet, consectetur adipiscing elit.\nLorem ipsum dolor sit amet, consectetur adipiscing elit.\nLorem ipsum dolor sit amet, consectetur adipiscing elit.',
-                  style: GoogleFonts.aleo(
-                      color: Colors.black, fontSize: 12.0, height: 1.5),
-                )),
-          ),
-          Expanded(
-            flex: 3,
-            child: Container(
-              margin: const EdgeInsets.symmetric(horizontal: 30.0),
-              decoration: BoxDecoration(
-                color: CustomColors.greyAccent,
-                borderRadius: BorderRadius.circular(20.0),
-              ),
-              child: MarkdownWidget(
-                selectable: true,
-                data: markdownSource,
-                physics: const BouncingScrollPhysics(),
-                padding: const EdgeInsets.all(20.0),
-                styleConfig: StyleConfig(
-                  preConfig: PreConfig(
-                    decoration: const BoxDecoration(
-                      color: Colors.transparent,
-                    ),
-                    padding: const EdgeInsets.all(0.0),
-                    language: 'dart',
-                    textStyle: GoogleFonts.ubuntuMono().copyWith(
-                      fontSize: 14.0,
-                    ),
-                    theme: theme.a11yLightTheme,
+      body: Consumer(
+        builder: (context, ref, child) {
+          AsyncValue<CodeModel?> codeData =
+              ref.watch(codeStateNotifierProvider);
+          return codeData.when(
+            data: (data) {
+              List<CodeDatum> dataList = data!.codeData;
+              return Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 20.0),
+                    child: RichText(
+                        overflow: TextOverflow.clip,
+                        textAlign: TextAlign.start,
+                        textDirection: TextDirection.ltr,
+                        softWrap: true,
+                        text: TextSpan(
+                          text: dataList[args.index].description,
+                          style: GoogleFonts.aleo(
+                              color: Colors.black, fontSize: 12.0, height: 1.5),
+                        )),
                   ),
-                ),
-              ),
-            ),
-            //TODO: create code item model
-            //TODO: add data (code or image link) and description property to code item model
-            //TODO: implement photo data instead of code snippet
-            // child: ClipRRect(
-            //   borderRadius: BorderRadius.circular(20.0),
-            //   child: Image.asset('assets/images/sample.png'),
-            // ),
-          ),
-          const SizedBox(height: 20.0),
-          Expanded(
-            flex: 1,
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 110.0),
-                  child: ButtonWidget(
-                    onPressed: () {},
-                    buttonHeight: 60.0,
-                    buttonWidth: 30.0,
-                    borderRadius: 20.0,
-                    color: CustomColors.secondary,
-                    textWidget: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                  Expanded(
+                    flex: 3,
+                    child: dataList[args.index].isCode
+                        ? Container(
+                            margin:
+                                const EdgeInsets.symmetric(horizontal: 30.0),
+                            decoration: BoxDecoration(
+                              color: CustomColors.greyAccent,
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
+                            child: MarkdownWidget(
+                              selectable: true,
+                              data: dataList[args.index].data,
+                              physics: const BouncingScrollPhysics(),
+                              padding: const EdgeInsets.all(20.0),
+                              styleConfig: StyleConfig(
+                                preConfig: PreConfig(
+                                  decoration: const BoxDecoration(
+                                    color: Colors.transparent,
+                                  ),
+                                  padding: const EdgeInsets.all(0.0),
+                                  language: 'dart',
+                                  textStyle: GoogleFonts.ubuntuMono().copyWith(
+                                    fontSize: 14.0,
+                                  ),
+                                  theme: theme.a11yLightTheme,
+                                ),
+                              ),
+                            ),
+                          )
+                        : ClipRRect(
+                            borderRadius: BorderRadius.circular(20.0),
+                            child: Image.network(dataList[args.index].data),
+                          ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  Expanded(
+                    flex: 1,
+                    child: Column(
                       children: <Widget>[
-                        Image.asset(
-                          'assets/icons/send.png',
-                          scale: 25.0,
+                        Padding(
+                          padding:
+                              const EdgeInsets.symmetric(horizontal: 110.0),
+                          child: ButtonWidget(
+                            onPressed: () {},
+                            buttonHeight: 60.0,
+                            buttonWidth: 30.0,
+                            borderRadius: 20.0,
+                            color: CustomColors.secondary,
+                            textWidget: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Image.asset(
+                                  'assets/icons/send.png',
+                                  scale: 25.0,
+                                ),
+                                const SizedBox(width: 10.0),
+                                const BoldTextWidget(
+                                  color: Colors.white,
+                                  fontSize: 14.0,
+                                  text: 'Share Code',
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
-                        const SizedBox(width: 10.0),
-                        const BoldTextWidget(
-                          color: Colors.white,
-                          fontSize: 14.0,
-                          text: 'Share Code',
+                        TextButton(
+                          onPressed: () {},
+                          style: ButtonStyle(
+                              overlayColor: MaterialStateProperty.all(
+                                  Colors.transparent)),
+                          child: const BoldTextWidget(
+                            color: CustomColors.primary,
+                            fontSize: 14.0,
+                            text: 'Preview',
+                            decoration: TextDecoration.underline,
+                          ),
                         ),
                       ],
                     ),
                   ),
-                ),
-                TextButton(
-                  onPressed: () {},
-                  style: ButtonStyle(
-                      overlayColor:
-                          MaterialStateProperty.all(Colors.transparent)),
-                  child: const BoldTextWidget(
-                    color: CustomColors.primary,
-                    fontSize: 14.0,
-                    text: 'Preview',
-                    decoration: TextDecoration.underline,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+                ],
+              );
+            },
+            error: (error, stackTrace) => Text(error.toString()),
+            loading: () => const Center(
+                child: CircularProgressIndicator(
+              color: CustomColors.primary,
+            )),
+          );
+        },
       ),
     );
   }
